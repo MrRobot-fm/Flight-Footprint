8c1fde55beda345fc60388bb663a3a3c33eef775
mrrobot <federicomigliore87@gmail.comm>
2023-01-09 14:20:03 +0100

    fix

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -1,26 +1,9 @@
-import { useState, useEffect } from 'react';
-import iphone from '../assets/img/Silver.png';
 import screen from '../assets/img/Screen2.png';
 import { ScreenData } from '.';
 import { useSelector } from 'react-redux';
 
 const Screen = ({ active }) => {
   const { footprint } = useSelector(store => store.footprint);
-  const getOrientation = () =>
-    window.matchMedia('(orientation:landscape)').matches;
-  const [orientation, setOrientation] = useState(
-    getOrientation() ? 'landscape-primary' : 'portrait-primary'
-  );
-  console.log(orientation);
-  useEffect(() => {
-    const updateOrientation = event => {
-      setOrientation(getOrientation());
-    };
-    window.addEventListener('orientationchange', updateOrientation);
-    return () => {
-      window.removeEventListener('orientationchange', updateOrientation);
-    };
-  }, []);
 
   return (
     <div
@@ -30,14 +13,8 @@ const Screen = ({ active }) => {
           : 'ss:translate-x-[320px] translate-x-[200px] opacity-[0.5] ease-in-out duration-1000 '
       }  flex justify-center items-center relative md:mt-0 mt-16`}
     >
-      <img
-        src={orientation === 'landscape-primary' ? iphone : screen}
-        alt="ipad-screen"
-        className={
-          orientation === 'landscape-primary' ? ' w-[17rem] ' : 'w-[30rem] '
-        }
-      ></img>
-      {active ? <ScreenData orientation={orientation} /> : null}
+      <img src={screen} alt="ipad-screen" className="w-[26rem] "></img>
+      {active ? <ScreenData /> : null}
     </div>
   );
 };
--- src/components/ScreenData.jsx
+++ src/components/ScreenData.jsx
@@ -1,7 +1,7 @@
 import { useSelector } from 'react-redux';
 import { HiOutlineArrowNarrowRight } from 'react-icons/hi';
 
-const ScreenData = ({ orientation }) => {
+const ScreenData = () => {
   const { queryCodeFrom, queryCodeTo, passenger, footprint } = useSelector(
     store => store.footprint
   );
@@ -10,65 +10,29 @@ const ScreenData = ({ orientation }) => {
       {queryCodeFrom && queryCodeTo && passenger && footprint ? (
         <>
           <div className={`flex flex-col w-full font-semibold items-center `}>
-            <h5
-              className={`${
-                orientation === 'landscape-primary'
-                  ? 'text-[1.3rem] text-primary'
-                  : 'xs:text-[2.2rem] text-[1.5rem] text-primary'
-              }`}
-            >
+            <h5 className="xs:text-[2.2rem] text-[1.5rem] text-primary">
               Flight:
             </h5>
-            <div
-              className={`${
-                orientation === 'landscape-primary'
-                  ? 'text-[1.8rem] text-secondary'
-                  : 'xs:text-[2.8rem] text-[2rem] text-secondary'
-              }  flex items-center w-[65%] justify-center gap-2 `}
-            >
+            <div className="xs:text-[2.8rem] text-[2rem] text-secondary  flex items-center w-[65%] justify-center gap-2 ">
               <span>{queryCodeFrom.substr(0, 3)} </span>
               <HiOutlineArrowNarrowRight />
               <span> {queryCodeTo.substr(0, 3)}</span>
             </div>
           </div>
           <div className="flex flex-col items-center font-semibold">
-            <h5
-              className={`${
-                orientation === 'landscape-primary'
-                  ? 'text-[1.3rem] text-primary'
-                  : 'xs:text-[2.2rem] text-[1.4rem] text-primary'
-              }`}
-            >
+            <h5 className="xs:text-[2.2rem] text-[1.4rem] text-primary">
               Total CO2:
             </h5>
 
-            <span
-              className={` ${
-                orientation === 'landscape-primary'
-                  ? 'text-[2rem]'
-                  : 'xs:text-[2.7rem] text-[2rem]'
-              } text-secondary `}
-            >
+            <span className="xs:text-[2.7rem] text-[2rem] text-secondary">
               {footprint.co2e.toFixed(1)} {footprint.co2e_unit}
             </span>
           </div>
           <div className="flex flex-col items-center font-semibold">
-            <h5
-              className={`${
-                orientation === 'landscape-primary'
-                  ? 'text-[1.3rem] text-primary'
-                  : 'xs:text-[2.2rem] text-[1.4rem] text-primary'
-              }`}
-            >
+            <h5 className="xs:text-[2.2rem] text-[1.4rem] text-primary">
               CO2/passenger:
             </h5>
-            <span
-              className={` ${
-                orientation === 'landscape-primary'
-                  ? 'text-[2rem]'
-                  : 'xs:text-[2.7rem] text-[2rem]'
-              } text-secondary `}
-            >
+            <span className="xs:text-[2.7rem] text-[2rem] text-secondary ">
               {(footprint.co2e / passenger).toFixed(1)} {footprint.co2e_unit}
             </span>
           </div>
