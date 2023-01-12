a7431c458b00e1b430658ac100655b2c1ad6466b
mrrobot <federicomigliore87@gmail.comm>
2023-01-12 12:40:15 +0100

    fix

--- src/components/Screen.jsx
+++ src/components/Screen.jsx
@@ -8,7 +8,7 @@ const Screen = ({ active }) => {
         active
           ? 'translate-x-[0px] opacity-100 ease-in-out duration-[1.5s] '
           : 'ss:translate-x-[320px] translate-x-[200px] opacity-[0.5] ease-in-out duration-1000 '
-      }  flex justify-center items-center relative md:mt-0 mt-16`}
+      }  flex justify-center items-center relative md:mt-0 mt-16 overflow-hidden`}
     >
       <img src={iphone} alt="ipad-screen" className="w-[18rem] "></img>
       {active ? <ScreenData /> : null}
